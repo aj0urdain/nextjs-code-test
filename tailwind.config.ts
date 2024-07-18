75ef3e2205370c7c6d2e65f3556f7aab74491d25
ajkfreak <52364128+aj0urdain@users.noreply.github.com>
2024-07-19 00:00:49 +1000

    feat(config): add animation to tailwind config
    Extend Tailwind CSS configuration to include custom keyframes and animation for slideInFromTop effect.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,17 @@ const config: Config = {
 			center: true,
 			padding: '1rem',
 		},
-		extend: {},
+		extend: {
+			keyframes: {
+				slideInFromTop: {
+					'0%': {transform: 'translateY(-5%)', opacity: '0%'},
+					'100%': {transform: 'translateY(0%)', opacity: '100%'},
+				},
+			},
+			animation: {
+				slideInFromTop: '0.3s ease-out 0s 1 slideInFromTop',
+			},
+		},
 	},
 	plugins: [
 		tailwindcssAnimate,
